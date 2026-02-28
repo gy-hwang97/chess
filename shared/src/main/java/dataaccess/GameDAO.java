92a5b5d839e7316dd10e227b7c46e6181f09d679
Gilyeon Hwang <79456022+gy-hwang97@users.noreply.github.com>
2026-02-27 22:59:20 -0700

    Create GameDAO.java

---
+++ shared/src/main/java/dataaccess/GameDAO.java
@@ -0,0 +1,12 @@
+package dataaccess;
+
+import java.util.List;
+import model.GameData;
+
+public interface GameDAO {
+    void clear() throws DataAccessException;
+    int createGame(GameData game) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    List<GameData> listGames() throws DataAccessException;
+    void updateGame(GameData game) throws DataAccessException;
+}
