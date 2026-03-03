05a9272355d95b6eafddc71b23a4b59ba6d11dab
Gilyeon Hwang <79456022+gy-hwang97@users.noreply.github.com>
2026-03-02 22:10:24 -0700

    Create RegisterService.java

---
+++ shared/src/main/java/service/RegisterService.java
@@ -0,0 +1,44 @@
+package service;
+
+import java.util.UUID;
+import dataaccess.UserDAO;
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import model.UserData;
+import model.AuthData;
+
+public class RegisterService {
+    private UserDAO userDAO;
+    private AuthDAO authDAO;
+
+    public RegisterService(UserDAO userDAO, AuthDAO authDAO) {
+        this.userDAO = userDAO;
+        this.authDAO = authDAO;
+    }
+
+    public RegisterResult register(RegisterRequest request) throws DataAccessException, ServiceException {
+        if (request == null) {
+            throw new ServiceException(400, "Error: bad request");
+        }
+        if (request.username() == null || request.password() == null || request.email() == null) {
+            throw new ServiceException(400, "Error: bad request");
+        }
+        if (request.username().isBlank() || request.password().isBlank() || request.email().isBlank()) {
+            throw new ServiceException(400, "Error: bad request");
+        }
+
+        UserData oldUser = userDAO.getUser(request.username());
+        if (oldUser != null) {
+            throw new ServiceException(403, "Error: already taken");
+        }
+
+        UserData newUser = new UserData(request.username(), request.password(), request.email());
+        userDAO.createUser(newUser);
+
+        String token = UUID.randomUUID().toString();
+        AuthData auth = new AuthData(token, request.username());
+        authDAO.createAuth(auth);
+
+        return new RegisterResult(request.username(), token);
+    }
+}
