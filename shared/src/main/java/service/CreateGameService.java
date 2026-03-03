9437ad95199563c747f642034808ac620b70f19b
Gilyeon Hwang <79456022+gy-hwang97@users.noreply.github.com>
2026-03-02 22:12:50 -0700

    Create CreateGameService.java

---
+++ shared/src/main/java/service/CreateGameService.java
@@ -0,0 +1,38 @@
+package service;
+
+import chess.ChessGame;
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import dataaccess.DataAccessException;
+import model.AuthData;
+import model.GameData;
+
+public class CreateGameService {
+    private AuthDAO authDAO;
+    private GameDAO gameDAO;
+
+    public CreateGameService(AuthDAO authDAO, GameDAO gameDAO) {
+        this.authDAO = authDAO;
+        this.gameDAO = gameDAO;
+    }
+
+    public CreateGameResult createGame(String authToken, CreateGameRequest request) throws DataAccessException, ServiceException {
+        if (authToken == null || authToken.isBlank()) {
+            throw new ServiceException(401, "Error: unauthorized");
+        }
+
+        AuthData auth = authDAO.getAuth(authToken);
+        if (auth == null) {
+            throw new ServiceException(401, "Error: unauthorized");
+        }
+
+        if (request == null || request.gameName() == null || request.gameName().isBlank()) {
+            throw new ServiceException(400, "Error: bad request");
+        }
+
+        GameData game = new GameData(0, null, null, request.gameName(), new ChessGame());
+        int gameID = gameDAO.createGame(game);
+
+        return new CreateGameResult(gameID);
+    }
+}
