d7fea65d02253cc65044a830aacadb5392f10c90
Gilyeon Hwang <79456022+gy-hwang97@users.noreply.github.com>
2026-03-02 22:54:56 -0700

    Create JoinGameServiceTest.java

---
+++ server/src/test/java/service/JoinGameServiceTest.java
@@ -0,0 +1,47 @@
+package service;
+
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import model.AuthData;
+import model.GameData;
+import chess.ChessGame;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class JoinGameServiceTest {
+
+    @Test
+    public void joinGamePositiveTest() throws Exception {
+        MemoryAuthDAO authDAO = new MemoryAuthDAO();
+        MemoryGameDAO gameDAO = new MemoryGameDAO();
+
+        authDAO.createAuth(new AuthData("t1", "u1"));
+        int gameID = gameDAO.createGame(new GameData(0, null, null, "g1", new ChessGame()));
+
+        JoinGameService service = new JoinGameService(authDAO, gameDAO);
+        service.joinGame("t1", new JoinGameRequest("WHITE", gameID));
+
+        GameData game = gameDAO.getGame(gameID);
+        assertEquals("u1", game.whiteUsername());
+    }
+
+    @Test
+    public void joinGameNegativeTest() throws Exception {
+        MemoryAuthDAO authDAO = new MemoryAuthDAO();
+        MemoryGameDAO gameDAO = new MemoryGameDAO();
+
+        authDAO.createAuth(new AuthData("t1", "u1"));
+        authDAO.createAuth(new AuthData("t2", "u2"));
+
+        int gameID = gameDAO.createGame(new GameData(0, null, null, "g1", new ChessGame()));
+
+        JoinGameService service = new JoinGameService(authDAO, gameDAO);
+        service.joinGame("t1", new JoinGameRequest("WHITE", gameID));
+
+        assertThrows(ServiceException.class, () -> {
+            service.joinGame("t2", new JoinGameRequest("WHITE", gameID));
+        });
+    }
+}
