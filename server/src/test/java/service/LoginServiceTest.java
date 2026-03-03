028ec0c19feab7865e6eb50648c6c7ed5000b4e2
Gilyeon Hwang <79456022+gy-hwang97@users.noreply.github.com>
2026-03-02 22:53:55 -0700

    Create LoginServiceTest.java

---
+++ server/src/test/java/service/LoginServiceTest.java
@@ -0,0 +1,41 @@
+package service;
+
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryUserDAO;
+import model.UserData;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class LoginServiceTest {
+
+    @Test
+    public void loginPositiveTest() throws Exception {
+        MemoryUserDAO userDAO = new MemoryUserDAO();
+        MemoryAuthDAO authDAO = new MemoryAuthDAO();
+
+        userDAO.createUser(new UserData("u1", "p1", "e1"));
+
+        LoginService service = new LoginService(userDAO, authDAO);
+        LoginResult result = service.login(new LoginRequest("u1", "p1"));
+
+        assertEquals("u1", result.username());
+        assertNotNull(result.authToken());
+    }
+
+    @Test
+    public void loginNegativeTest() throws Exception {
+        MemoryUserDAO userDAO = new MemoryUserDAO();
+        MemoryAuthDAO authDAO = new MemoryAuthDAO();
+
+        userDAO.createUser(new UserData("u1", "p1", "e1"));
+
+        LoginService service = new LoginService(userDAO, authDAO);
+
+        assertThrows(ServiceException.class, () -> {
+            service.login(new LoginRequest("u1", "wrong"));
+        });
+    }
+}
