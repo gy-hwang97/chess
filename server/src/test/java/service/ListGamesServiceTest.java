087d4e3e763734bb94f30a1b1c7cd4afc3e59ee0
Gilyeon Hwang <79456022+gy-hwang97@users.noreply.github.com>
2026-03-03 22:27:43 -0700

    Update ListGamesServiceTest.java

--- server/src/test/java/service/ListGamesServiceTest.java
+++ server/src/test/java/service/ListGamesServiceTest.java
@@ -6,6 +6,7 @@ import model.AuthData;
 import model.GameData;
 import chess.ChessGame;
 import org.junit.jupiter.api.Test;
+import model.result.ListGamesResult;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
