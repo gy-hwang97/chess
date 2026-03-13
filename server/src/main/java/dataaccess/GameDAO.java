81942eee8dca1b2028471519634108272deaae3d
dmdsiz <kevin2934@gmail.com>
2026-03-12 22:35:50 -0600

    phase 4

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,13 +1,17 @@
 package dataaccess;
 
 import model.GameData;
-
-import java.util.Collection;
+import java.util.List;
 
 public interface GameDAO {
-    void clear();
-    int createGame(GameData game);
-    GameData getGame(int gameID);
-    Collection<GameData> listGames();
+
+    void clear() throws DataAccessException;
+
+    int createGame(GameData game) throws DataAccessException;
+
+    GameData getGame(int gameID) throws DataAccessException;
+
+    List<GameData> listGames() throws DataAccessException;
+
     void updateGame(GameData game) throws DataAccessException;
 }
