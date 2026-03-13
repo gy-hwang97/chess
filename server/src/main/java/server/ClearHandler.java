de21b6dc0ce27e1abc4e81b51875026495951589
dmdsiz <kevin2934@gmail.com>
2026-03-12 21:50:06 -0600

    Fix ClearHandler exception handling

--- server/src/main/java/server/ClearHandler.java
+++ server/src/main/java/server/ClearHandler.java
@@ -3,6 +3,7 @@ package server;
 import com.google.gson.Gson;
 import io.javalin.http.Context;
 import service.ClearService;
+import service.ServiceException;
 
 public class ClearHandler {
     private final ClearService clearService;
@@ -13,8 +14,16 @@ public class ClearHandler {
     }
 
     public void clear(Context ctx) {
-        clearService.clear();
-        ctx.status(200);
-        ctx.result(gson.toJson(new EmptyResponse()));
+        try {
+            clearService.clear();
+            ctx.status(200);
+            ctx.result(gson.toJson(new EmptyResponse()));
+        } catch (ServiceException e) {
+            ctx.status(e.statusCode());
+            ctx.result(gson.toJson(new ErrorResponse(e.getMessage())));
+        } catch (Exception e) {
+            ctx.status(500);
+            ctx.result(gson.toJson(new ErrorResponse("Error: " + e.getMessage())));
+        }
     }
 }
