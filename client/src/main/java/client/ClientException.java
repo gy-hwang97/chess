09f4a92d52a178dce081f7be3d9531409ed1d063
dmdsiz <kevin2934@gmail.com>
2026-03-23 23:39:11 -0600

    created ClientException.java

---
+++ client/src/main/java/client/ClientException.java
@@ -0,0 +1,14 @@
+package client;
+
+public class ClientException extends Exception {
+    private final int statusCode;
+
+    public ClientException(int statusCode, String message) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+}
