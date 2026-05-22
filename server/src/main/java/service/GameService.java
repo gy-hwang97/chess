d629447e4c21b6c2a4095340373b019e1ae47235
Gilyeon Hwang <79456022+gy-hwang97@users.noreply.github.com>
2026-05-21 18:55:44 -0600

    Update GameService.java

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,5 +1,6 @@
 package service;
 
+import dataaccess.AlreadyTakenException;
 import dataaccess.AuthDAO;
 import dataaccess.BadRequestException;
 import dataaccess.DataAccessException;
@@ -46,5 +47,45 @@ public class GameService {
 
         int gameID = gameDAO.createGame(request.gameName());
         return new CreateGameResult(gameID);
+    }
+
+    public void joinGame(String authToken, JoinGameRequest request)
+            throws DataAccessException, UnauthorizedException, BadRequestException, AlreadyTakenException {
+
+        AuthData auth = authDAO.getAuth(authToken);
+        if (auth == null) {
+            throw new UnauthorizedException("unauthorized");
+        }
+
+        if (request.playerColor() == null) {
+            throw new BadRequestException("bad request");
+        }
+        if (!request.playerColor().equals("WHITE") && !request.playerColor().equals("BLACK")) {
+            throw new BadRequestException("bad request");
+        }
+
+        GameData game = gameDAO.getGame(request.gameID());
+        if (game == null) {
+            throw new BadRequestException("bad request");
+        }
+
+        String username = auth.username();
+        GameData updatedGame;
+
+        if (request.playerColor().equals("WHITE")) {
+            if (game.whiteUsername() != null) {
+                throw new AlreadyTakenException("already taken");
+            }
+            updatedGame = new GameData(game.gameID(), username, game.blackUsername(),
+                    game.gameName(), game.game());
+        } else {
+            if (game.blackUsername() != null) {
+                throw new AlreadyTakenException("already taken");
+            }
+            updatedGame = new GameData(game.gameID(), game.whiteUsername(), username,
+                    game.gameName(), game.game());
+        }
+
+        gameDAO.updateGame(updatedGame);
     }
 }
