35db4442d6851ba201a7d285815ef42b5a6a7ac4
gy-hwang97 <gilyeonhwang97@gmail.com>
2026-06-02 23:21:28 -0600

    phase5-7

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,10 +1,14 @@
 package client;
 
-import chess.*;
+import ui.Repl;
 
 public class ClientMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        int port = 8080;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        }
+        Repl repl = new Repl(port);
+        repl.run();
     }
 }
