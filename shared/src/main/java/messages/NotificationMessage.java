80ad6cf6554e17056c207b11ebb2ced70f00481f
gy-hwang97 <gilyeonhwang97@gmail.com>
2026-06-09 21:51:09 -0600

    phase 6-10

---
+++ shared/src/main/java/messages/NotificationMessage.java
@@ -0,0 +1,15 @@
+package messages;
+
+public class NotificationMessage extends ServerMessage {
+
+    private final String message;
+
+    public NotificationMessage(String message) {
+        super(ServerMessageType.NOTIFICATION);
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
